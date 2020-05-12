1185b02f3d2f579adf9c49632645b6e1df665e48
Samuel Caladas <samuel.caldas@gmail.com>
2020-05-12 15:07:06 -0300

    Initial commit

---
+++ inserir.php
@@ -0,0 +1,25 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>inserir</title>
+</head>
+
+<body>
+<ul>
+<?php
+while (($nome_itens = readdir(opendir(getcwd().'/tabs'))) !== false) {
+    $itens[] = $nome_itens;
+}
+sort($itens);
+foreach ($itens as $listar) {
+	if ($listar!="." && $listar!=".."){
+		if (!is_dir($listar)){ 
+			echo "<li>".$listar."</li>";
+		}
+   }
+}
+?>
+</ul>
+</body>
+</html>
